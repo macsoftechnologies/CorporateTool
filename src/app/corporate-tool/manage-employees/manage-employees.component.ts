ce26811aa76887087cbc056fe03f60df75c3d268
suresh411 <macsoftechnologies@gmail.com>
2020-02-10 15:49:11 +0530

    design manage employee

--- src/app/corporate-tool/manage-employees/manage-employees.component.ts
+++ src/app/corporate-tool/manage-employees/manage-employees.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manage-employees',
   templateUrl: './manage-employees.component.html',
-  styleUrls: ['./manage-employees.component.css']
+  styleUrls: ['./manage-employees.component.scss']
 })
 export class ManageEmployeesComponent implements OnInit {
 
