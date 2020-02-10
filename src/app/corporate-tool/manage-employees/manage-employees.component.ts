7da654bd364bbdb51ce6b5111a3cebcab0a7f23a
Lohith .s <macsoftechnologies@gmail.com>
2020-02-10 15:47:24 +0530

    added Stepper

--- src/app/corporate-tool/manage-employees/manage-employees.component.ts
+++ src/app/corporate-tool/manage-employees/manage-employees.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
+
 
 @Component({
   selector: 'app-manage-employees',
@@ -9,7 +11,26 @@ export class ManageEmployeesComponent implements OnInit {
 
   constructor() { }
 
+  name = 'Angular';
+  private stepper: Stepper;
+
+  previous(){
+    this.stepper.previous();
+  }
+
+  next() {
+    this.stepper.next();
+  }
+
+  onSubmit() {
+    return false;
+  }
+
   ngOnInit() {
+    this.stepper = new Stepper(document.querySelector('#stepper1'), {
+      linear: false,
+      animation: true
+    })
   }
 
 }
