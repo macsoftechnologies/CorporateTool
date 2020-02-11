d590c8d08a7eaad5abea6b16b3d765aef3c8c621
suresh411 <macsoftechnologies@gmail.com>
2020-02-11 15:22:52 +0530

    loyaltycarddetails updated

--- src/app/corporate-tool/corporatetool.module.ts
+++ src/app/corporate-tool/corporatetool.module.ts
@@ -5,10 +5,18 @@ import { RouterModule } from "@angular/router";
 import { MasterComponent } from './master/master.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ManageEmployeesComponent } from './manage-employees/manage-employees.component';
+import { FormsModule } from '@angular/forms';
+// import { BrowserModule  } from '@angular/platform-browser';
+import { CommonModule  } from '@angular/common';
+
+
 
 @NgModule({ imports: [
     RouterModule.forChild(corporateToolRoutes),
-    NgbModule
+    NgbModule,
+    FormsModule,
+    // BrowserModule,
+    CommonModule
 ],
 declarations: [ManageCompanyComponent, MasterComponent, ManageEmployeesComponent]
 })
