1edf3bbf25dc98615e4265ff2d22573975c20ad6
suresh411 <macsoftechnologies@gmail.com>
2020-02-13 18:21:10 +0530

    company preference

--- src/app/corporate-tool/company-preferences/company-preferences.component.ts
+++ src/app/corporate-tool/company-preferences/company-preferences.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormControl, Validators } from '@angular/forms';
+import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-company-preferences',
   templateUrl: './company-preferences.component.html',
-  styleUrls: ['./company-preferences.component.css']
+  styleUrls: ['./company-preferences.component.scss']
 })
 export class CompanyPreferencesComponent implements OnInit {
+  PreferredAirlinesobject={};
+  PreferredAirlinesarray=[];
+  selectairline=[
+    {code:"sa",airlinename:"Select Airline Name"},
+    {code:"AI",airlinename:"Air india"},
+    {code:"IX",airlinename:"Air India Express"},
+    {code:"QM",airlinename:"Air Malawi"},
+    {code:"OB",airlinename:"Blue Air"},
+    {code:"LH",airlinename:"Deutsche luthansa"},
+
+  ];
+  PreferredAirlines = new FormGroup({
+    airline : new FormControl(this.selectairline[0].airlinename,Validators.required)
+  });
+
+  onsubmit(){
+    this.PreferredAirlinesobject=this.PreferredAirlines.value;
+    this.PreferredAirlinesarray.push(this.PreferredAirlinesobject);
+    this.PreferredAirlines.patchValue({
+      airline:this.selectairline[0].airlinename,
+      
+    })
+    
+    
+  }
+
 
   constructor() { }
 
